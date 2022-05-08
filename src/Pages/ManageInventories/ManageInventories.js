653e3c788586b8f99bf7a0224ef5146338974a8e
nishattasnim08 <nishattasnim24258@gmail.com>
2022-05-08 17:25:15 +0600

    manage inventories page updated

--- src/Pages/ManageInventories/ManageInventories.js
+++ src/Pages/ManageInventories/ManageInventories.js
@@ -1,42 +1,52 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Table } from 'react-bootstrap';
 
 const ManageInventories = () => {
+
+    const [manageInventories, setManageInvetories] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setManageInvetories(data))
+    }, [])
+
     return (
         <div className='container my-5 text-center'>
             <div>
                 <h1>Manage Inventories</h1>
             </div>
-            <div>
-                <Table responsive>
+
+            <div className='devide'></div>
+
+            <div className='my-5 text-center'>
+                <Table striped bordered hover variant="dark">
                     <thead>
                         <tr>
                             <th>#</th>
-                            {Array.from({ length: 6 }).map((_, index) => (
-                                <th key={index}>Product Name</th>
-                            ))}
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Supplier Name</th>
+                            <th>Add Item</th>
+                            <th>Delete Item</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            {Array.from({ length: 6 }).map((_, index) => (
-                                <td key={index}>Table cell {index}</td>
-                            ))}
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            {Array.from({ length: 6 }).map((_, index) => (
-                                <td key={index}>Table cell {index}</td>
-                            ))}
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            {Array.from({ length: 6 }).map((_, index) => (
-                                <td key={index}>Table cell {index}</td>
-                            ))}
-                        </tr>
-                    </tbody>
+                    {manageInventories.map((manageInventory) => (
+                        <tbody>
+                            <tr>
+                                <td>{manageInventory.id}</td>
+                                <td>{manageInventory.name}</td>
+                                <td>{manageInventory.price}</td>
+                                <td>{manageInventory.quantity}</td>
+                                <td>{manageInventory.sName}</td>
+                            <th><Button variant="dark"><i class="fa-solid fa-square-plus"></i></Button></th>
+                            <th><Button variant="dark"><i class="fa-solid fa-trash"></i></Button></th>
+                            </tr>
+                            
+                        </tbody>
+                    ))}
+
                 </Table>
             </div>
         </div>
